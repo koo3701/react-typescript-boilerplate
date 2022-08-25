41ca2cc74cba1da4faa521a349c270b6e64d3293
koo3701 <koo.0107.koo@gmail.com>
2022-08-26 02:11:37 +0900

    add reactstrap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
 import { Interpolation, Theme } from '@emotion/react';
+import { useState } from 'react';
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from 'reactstrap';
 
 const styles: { [key: string]: Interpolation<Theme> } = {
-  wrap: {
-    textAlign: 'center',
-  },
   p: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  red: {
     color: '#FF0000',
+  },
+  green: {
+    color: '#00FF00',
+  },
+  blue: {
+    color: '#0000FF',
   },
 };
 
+const colors = ['red', 'green', 'blue'] as const;
+
 function App() {
+  const [color, setColor] = useState<typeof colors[number]>('red');
+  const [open, setOpen] = useState(false);
+
   return (
-    <div css={styles.wrap}>
-      <p css={styles.p}>react-typescript-boilerplate</p>
+    <div className='text-center'>
+      <p css={[styles.p, styles[color]]}>react-typescript-boilerplate</p>
+      <Dropdown isOpen={open} toggle={() => setOpen(!open)}>
+        <DropdownToggle caret>{color}</DropdownToggle>
+        <DropdownMenu>
+          {colors.map((color) => (
+            <DropdownItem onClick={() => setColor(color)}>{color}</DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 }
